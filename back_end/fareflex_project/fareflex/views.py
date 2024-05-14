cc00304535d5c67a1b6f9350d3dc9bd2c0774286
Muliro1 <mulirokhaemba@gmail.com>
2024-05-15 02:41:57 +0300

    debugging

--- back_end/fareflex_project/fareflex/views.py
+++ back_end/fareflex_project/fareflex/views.py
@@ -20,17 +20,27 @@ TEST_PUBLISHABLE_KEY = "ISPubKey_test_693bfb0f-b98c-4069-97e8-7927af92d66f"
 service = APIService(token=TEST_API_TOKEN, publishable_key=TEST_PUBLISHABLE_KEY, test=True)
 
 def create_wallet(request):
+    print(f"Request method: {request.method}")
     if request.method == 'POST':
         form = WalletForm(request.POST)
+        print(f"Form valid: {form.is_valid()}")
         if form.is_valid():
             label = form.cleaned_data.get('label')
             currency = form.cleaned_data.get('currency')
             can_disburse = form.cleaned_data.get('can_disburse')
             response = service.wallets.create(currency=currency, label=label, can_disburse=can_disburse)
-            if response.get('status') == 'success':
+            print(response.get('status'))
+            if response.get('status') == "success":
                 wallet_id = response.get('data').get('id')
-                Wallet.objects.create(wallet_id=wallet_id, label=label, currency=currency, can_disburse=can_disburse)
-                messages.success(request, 'Wallet successfully created.')
+                try:
+                    Wallet.objects.create(wallet_id=wallet_id, label=label, currency=currency, can_disburse=can_disburse)
+                    messages.success(request, 'Wallet successfully created.')
+                    #print(response)
+                except Exception as e:
+                        print(f"Error calling service.wallets.create: {e}")
+                        response = None
+            else:
+                print(f"API response status not success: {response.get('status')}")
     else:
         form = WalletForm()
         messages.error(request, 'Failed to create wallet.')
