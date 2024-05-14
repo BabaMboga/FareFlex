cc79ed2fa52b89b7207d6e7fc694304336b0167b
johnVundi <johnsonvundy2@gmail.com>
2024-05-14 21:58:19 +0300

    complete login and signup setup

--- front-end/src/components/AuthForms/EmailSignIn.tsx
+++ front-end/src/components/AuthForms/EmailSignIn.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useState } from "react";
-//import { signIn } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
