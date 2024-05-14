196657b3add3240f0ea7b3da17c56ddb5ffd20af
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2024-05-14 21:43:50 +0300

    test push

--- front-end/postcss.config.mjs
+++ front-end/postcss.config.mjs
@@ -2,10 +2,9 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 
-export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-    ...(process.env.NODE_ENV === 'production' ? [cssnano()] : []),
-  ],
+module.exports = {
+  plugins: {
+    tailwindcss: [],
+    autoprefixer: [],
+  },
 };
