876f9860bac99944b963ef6a78d9143f1aad3acc
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2024-05-15 10:11:08 +0300

    fixed layout error

--- front-end/postcss.config.mjs
+++ front-end/postcss.config.mjs
@@ -2,9 +2,9 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 
-module.exports = {
+export default {
   plugins: {
-    tailwindcss: [],
-    autoprefixer: [],
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
