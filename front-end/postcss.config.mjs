7963e0e226718f0d253dd2d013bb021eef59881d
johnVundi <johnsonvundy2@gmail.com>
2024-05-15 21:42:25 +0300

    fixing error

--- front-end/postcss.config.mjs
+++ front-end/postcss.config.mjs
@@ -2,9 +2,10 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 
-module.exports = {
-  plugins: {
-    tailwindcss: [],
-    autoprefixer: [],
-  },
+export default {
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+    cssnano(),
+  ],
 };
