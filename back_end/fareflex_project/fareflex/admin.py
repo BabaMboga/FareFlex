702f8421e49960898e574322e5539562e4104b19
Muliro1 <mulirokhaemba@gmail.com>
2024-05-15 10:14:34 +0300

    model registration

--- back_end/fareflex_project/fareflex/admin.py
+++ back_end/fareflex_project/fareflex/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import Wallet
+from .models import Wallet, Route, Vehicle, Transaction, PaymentMethod
 
 admin.site.register(Wallet)
+admin.site.register(Route)
+admin.site.register(Vehicle)
+admin.site.register(Transaction)
+admin.site.register(PaymentMethod)
