9273c400996e8d4d93d65328fa03f66eb2aec37c
Ayim William <odhisayim@gmail.com>
2024-05-16 21:15:52 +0300

    Made most changes

--- back-end/models.py
+++ back-end/models.py
@@ -7,6 +7,8 @@ class SerializerMixin(object):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
     
 class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True)
     user_type = db.Column(db.String(20))
@@ -17,28 +19,37 @@ class User(db.Model, SerializerMixin):
     wallet = db.relationship('Wallet', backref='user', uselist=False)
 
 class Wallet(db.Model, SerializerMixin):
+    __tablename__ = 'wallets'
+
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True)
     balance = db.Column(db.Numeric(10, 2))
     currnecy = db.Column(db.String(3))
     last_updated = db.Column(db.DateTime, default=db.func.now())
 
 class Transaction(db.Model, SerializerMixin):
+    __tablename__ = 'transactions'
+
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    wallet_id = db.Column(db.Integer, db.ForeignKey('wallet'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    wallet_id = db.Column(db.Integer, db.ForeignKey('wallets.id'))
     transaction_type = db.Column(db.String(10))
     amount = db.Column(db.Numeric(10, 2))
     description = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, default=db.func.now())
 
 class PaymentMethod(db.Model, SerializerMixin):
+    __tablename__ = 'paymentmethods'
+    
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     payment_type = db.Column(db.String(20))
     details = db.Column(db.Text)
 
 class Route(db.Model, SerializerMixin):
+    __tablename__ = 'routes'
+
+
     id = db.Column(db.Integer, primary_key=True)
     route_name = db.Column(db.String(100))
     start_location = db.Column(db.String(100))
@@ -47,8 +58,10 @@ class Route(db.Model, SerializerMixin):
     price = db.Column(db.Numeric(10, 2))
 
 class Vehicle(db.Model, SerializerMixin):
+    __tablename__ = 'vehicles'
+
     id = db.Column(db.Integer, primary_key=True)
-    route_id = db.Column(db.Integer, db.ForeignKey('route.id'))
+    route_id = db.Column(db.Integer, db.ForeignKey('routes.id'))
     vehicle_number = db.Column(db.String(20), unique=True)
     driver_name = db.Column(db.String(150))
     conductor_name = db.Column(db.String(150))
