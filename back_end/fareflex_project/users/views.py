83000117a1a034226cd542910652899c5ef855e9
Muliro1 <mulirokhaemba@gmail.com>
2024-05-17 22:55:15 +0300

    pay_fare

--- back_end/fareflex_project/users/views.py
+++ back_end/fareflex_project/users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from intasend import APIService
+
+TEST_API_TOKEN = "ISSecretKey_test_aea56f44-d051-4796-a075-759b2406bb19"
+TEST_PUBLISHABLE_KEY = "ISPubKey_test_693bfb0f-b98c-4069-97e8-7927af92d66f"
 
 # Create your views here.
 def register(request):
@@ -13,3 +17,13 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+def pay_fare(request):
+    publishable_key = "INTASEND_PUBLISHABLE_KEY"
+    service = APIService(token=TEST_API_TOKEN, publishable_key=TEST_PUBLISHABLE_KEY, test=True)
+
+    response = service.collect.checkout(phone_number=254727563415,
+                                    email="mulirokhaemba@gmail.com", amount=10, currency="KES", comment="Service Fees", redirect_url="http://example.com/thank-you")
+    payment_url = response.get("url")
+    return render(request, 'users/pay_fare.html', {'payment_url': payment_url})
+
